91665eb32d30a8da2523c53962161b0692ded219
AssistantIsa <usanaconisa@gmail.com>
2026-05-05 13:16:20 +0100

    force deploy: Tue May  5 01:16:20 PM +01 2026

--- main.py
+++ main.py
@@ -34,3 +34,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main_simple:app", host="0.0.0.0", port=port)
+# force deploy Tue May  5 01:16:20 PM +01 2026
